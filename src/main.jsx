5542e7e5b0d17a3478ad007b6d47298e74d59733
Agit Rahadian_22110006 <164508850+AgitRahadian-22110006@users.noreply.github.com>
2025-05-22 03:36:32 +0700

    m

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+// Tambahkan ini untuk SEO di React
+import { HelmetProvider } from 'react-helmet-async';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
 )
